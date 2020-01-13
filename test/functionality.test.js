2e49ea81e290415f0f11e1d083475dd397f8d237
Pooya Parsa <pyapar@gmail.com>
2020-01-13 22:14:21 +0100

    feat: watch components directory and rebuild (#6)
    * feat: watch components directory and rebuild
    
    * chore: improve tests
    
    * chore: fully mock builder
    
    * chore: remove old snapshot

---
+++ test/functionality.test.js
@@ -0,0 +1,20 @@
+const { setup, loadConfig, get } = require('@nuxtjs/module-test-utils')
+
+describe('module', () => {
+  let nuxt
+
+  beforeAll(async () => {
+    ({ nuxt } = (await setup(loadConfig(__dirname))))
+  }, 60000)
+
+  afterAll(async () => {
+    await nuxt.close()
+  })
+
+  test('render', async () => {
+    const html = await get('/')
+    expect(html).toContain('Works!')
+    expect(html).toContain('foo')
+    expect(html).toContain('bar')
+  })
+})
--- test/module.test.js
+++ test/module.test.js
@@ -1,20 +1,63 @@
-const { setup, loadConfig, get } = require('@nuxtjs/module-test-utils')
+const { join } = require('path')
+const { loadConfig, init } = require('@nuxtjs/module-test-utils')
+const chokidar = require('chokidar')
+const globby = require('globby')
+
+// Mock globby
+jest.mock('globby')
+const toGlobbyFiles = arr => arr.map(name => ({ name, path: join('/project/nuxt', name) }))
+globby.mockImplementation(() => toGlobbyFiles(['a.js', 'b.global.js', 'c.global.js']))
+
+// Mock chokidar
+const chokidarWatchers = []
+const mockWatcher = {
+  close: jest.fn(),
+  on (event, fn) {
+    chokidarWatchers.push({ event, fn })
+  }
+}
+chokidar.watch = () => mockWatcher
+const callChokidarEvent = eventName => Promise.all(chokidarWatchers.map(w => w.fn(eventName)))
 
 describe('module', () => {
-  let nuxt
+  let nuxt, builder
 
   beforeAll(async () => {
-    ({ nuxt } = (await setup(loadConfig(__dirname))))
-  }, 60000)
+    nuxt = await init(loadConfig(__dirname, undefined, { dev: true }))
+    builder = {
+      generateRoutesAndFiles: jest.fn()
+    }
+    await nuxt.callHook('build:before', builder)
+  })
 
   afterAll(async () => {
     await nuxt.close()
   })
 
-  test('render', async () => {
-    const html = await get('/')
-    expect(html).toContain('Works!')
-    expect(html).toContain('foo')
-    expect(html).toContain('bar')
+  test('discover components and pass as componentOptions', () => {
+    const plugin = nuxt.options.build.templates.find(p => p.dst.includes('global-components'))
+    expect(plugin.options).toMatchSnapshot()
+  })
+
+  test('watch: no rebuild if no matched files changed', async () => {
+    builder.generateRoutesAndFiles.mockClear()
+    await callChokidarEvent('add')
+    expect(builder.generateRoutesAndFiles).toHaveBeenCalledTimes(0)
+  })
+
+  test('watch:rebuild on add/remove', async () => {
+    builder.generateRoutesAndFiles.mockClear()
+    globby.mockImplementationOnce(() => toGlobbyFiles(['a.js', 'b.global.js']))
+
+    await callChokidarEvent('unlink')
+    expect(builder.generateRoutesAndFiles).toHaveBeenCalledTimes(1)
+  })
+
+  test('watch:no rebuild on other events', async () => {
+    builder.generateRoutesAndFiles.mockClear()
+    globby.mockImplementationOnce(() => toGlobbyFiles(['a.js', 'b.global.js']))
+
+    await callChokidarEvent('foo')
+    expect(builder.generateRoutesAndFiles).toHaveBeenCalledTimes(0)
   })
 })
