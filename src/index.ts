61376e6fe5642e2c36f8668a2cdc505c9e98ac08
Kevin Marrec <kevin@marrec.io>
2020-02-26 17:38:00 +0100

    chore: add beginning of dev watch logic

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import path from 'path'
+import chokidar from 'chokidar'
 // @ts-ignore
 import RuleSet from 'webpack/lib/RuleSet'
 import { Module } from '@nuxt/types'
@@ -10,14 +11,12 @@ export interface Options {
 }
 
 const componentsModule: Module<Options> = function (_moduleOptions) {
-  const extensions = ['vue', 'js', ...this.options.build!.additionalExtensions!]
   const dir = path.join(this.options!.srcDir!, 'components')
+  const extensions = ['vue', 'js', ...this.options.build!.additionalExtensions!]
+  const scanOptions = { dir, extensions }
 
   this.nuxt.hook('build:before', async () => {
-    const components: any[] = await scanComponents({
-      extensions,
-      dir
-    })
+    const components: any[] = await scanComponents(scanOptions)
 
     this.extendBuild((config) => {
       const { rules }: any = new RuleSet(config.module!.rules)
@@ -25,6 +24,23 @@ const componentsModule: Module<Options> = function (_moduleOptions) {
       vueRule.use.unshift({ loader: require.resolve('./loader'), options: { components } })
       config.module!.rules = rules
     })
+
+    // Watch components directory for dev mode
+    if (this.options.dev) {
+      const watcher = chokidar.watch(dir, this.options.watchers!.chokidar)
+      watcher.on('all', (eventName) => {
+        if (!['add', 'unlink'].includes(eventName)) {
+          // return
+        }
+
+        // TODO : Alter Webpack Loader options to make it correctly have the updated components list
+      })
+
+      // Close watcher on nuxt close
+      this.nuxt.hook('close', () => {
+        watcher.close()
+      })
+    }
   })
 }
 
