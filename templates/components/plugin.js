cea76fedbb74bc007cf3bec419aedc2e40756b2d
pooya parsa <pooya@pi0.ir>
2020-06-02 22:57:00 +0200

    refactor: simplify plugin template

--- templates/components/plugin.js
+++ templates/components/plugin.js
@@ -1,18 +1,13 @@
 import Vue from 'vue'
 <% const globalComponents = options.getComponents().filter(c => c.global && c.async) %>
 
+const globalComponents = {
 <%= globalComponents.map(c => {
   const exp = c.export === 'default' ? `c.default || c` : `c['${c.export}']`
-  return `const ${c.pascalName.replace(/^Lazy/, '')} = import('../${relativeToBuild(c.filePath)}' /* webpackChunkName: "${c.chunkName}" */).then(c => ${exp})`
+  return `  ${c.pascalName.replace(/^Lazy/, '')}: import('../${relativeToBuild(c.filePath)}' /* webpackChunkName: "${c.chunkName}" */).then(c => ${exp})`
 }).join('\n') %>
-
-
-const globalComponents = { <%= globalComponents.map(c => c.pascalName.replace(/^Lazy/, '')).join(', ') %> }
-
-export function install(Vue) {
-  for (const name in globalComponents) {
-    Vue.component(name, globalComponents[name])
-  }
 }
 
-Vue.use(install)
+for (const name in globalComponents) {
+    Vue.component(name, globalComponents[name])
+}
