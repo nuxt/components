3fc3709729310ea3e6d8a3005b8b951045903dfc
pooya parsa <pyapar@gmail.com>
2020-07-29 14:56:07 +0200

    fix: invalid `webpackChunkName` for global exports

--- templates/components/index.js
+++ templates/components/index.js
@@ -5,5 +5,5 @@
 
 <%= options.getComponents().filter(c => c.async).map(c => {
   const exp = c.export === 'default' ? `c.default || c` : `c['${c.export}']`
-  return `export const ${c.pascalName} = import('../${relativeToBuild(c.filePath)}' /* webpackChunkName: "${c.chunkName}'}" */).then(c => ${exp})`
+  return `export const ${c.pascalName} = import('../${relativeToBuild(c.filePath)}' /* webpackChunkName: "${c.chunkName}" */).then(c => ${exp})`
 }).join('\n') %>
