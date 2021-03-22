b6c1f02993e03b5a6b8d614a8d8771e7b028d86d
Daniel Roe <daniel@roe.dev>
2021-03-22 15:06:06 +0000

    fix: use wrapper for lazy-loaded functional components
    * closes https://github.com/nuxt/nuxt.js/issues/8607

--- templates/components/plugin.js
+++ templates/components/plugin.js
@@ -1,10 +1,31 @@
 import Vue from 'vue'
 <% const components = options.getComponents() %>
 
+const getWrapperFor = (name) => (options) => {
+  if (!options.functional) return options
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    name: `${name}Wrapper`,
+    render(h) {
+      const attrs = Object.fromEntries(Object.entries(this.$attrs).filter(([key]) => !propKeys.includes(key)))
+      const props = Object.fromEntries(Object.entries(this.$attrs).filter(([key]) => propKeys.includes(key)))
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
+
 const components = {
 <%= components.map(c => {
   const exp = c.export === 'default' ? `c.default || c` : `c['${c.export}']`
-  return `  ${c.pascalName.replace(/^Lazy/, '')}: () => import('../${relativeToBuild(c.filePath)}' /* webpackChunkName: "${c.chunkName}" */).then(c => ${exp})`
+  return `  ${c.pascalName.replace(/^Lazy/, '')}: () => import('../${relativeToBuild(c.filePath)}' /* webpackChunkName: "${c.chunkName}" */).then(c => ${exp}).then(getWrapperFor('${c.pascalName}'))`
 }).join(',\n') %>
 }
 
