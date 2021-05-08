ec64251b9db3c396bbc8c3e435f7b77b5404006f
Jonas Galvez <jonasgalvez@gmail.com>
2021-05-08 16:36:49 -0300

    add component wrapper for svgs

--- templates/components/plugin.js
+++ templates/components/plugin.js
@@ -17,19 +17,26 @@ for (const c of rawComponents) {
 const components = {
 <%= components.map(c => {
   const exp = c.export === 'default' ? `c.default || c` : `c['${c.export}']`
-  return `  ${
-    c.pascalName.replace(/^Lazy/, '')
-  }: () => import('../${
-    relativeToBuild(c.filePath)
-  }' /* webpackChunkName: "${
-    c.chunkName
-  }" */).then(c => wrapFunctional(${
-    exp
-  }))`
+  return `  ${c.pascalName.replace(/^Lazy/, '')}: () => import('../${relativeToBuild(c.filePath)}' /* webpackChunkName: "${c.chunkName}" */).then(c => wrapFunctional(${exp}))`
 }).join(',\n') %>
 }
+
+const svgs = {}
+
+<% for (const c of svgs) { %>
+svgs[`<%= c.pascalName %>`] = {
+  render (h) {
+    const src = require('../<%= relativeToBuild(c.filePath) %>' /* webpackChunkName: "<%= c.chunkName %>" */)
+    return h('img', { attrs: { src } })
+  }
+}
+<% } %>
 
 for (const name in components) {
   Vue.component(name, components[name])
   Vue.component('Lazy' + name, components[name])
+}
+
+for (const name in svgs) {
+  Vue.component(name, svgs[name])
 }
