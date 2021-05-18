44b4c4cec8d3f67aadeaa3b072ccc5175e32de52
Nozomu Ikuta <nick.0508.nick@gmail.com>
2021-05-18 22:58:48 +0900

    fix: typo

--- templates/components/plugin.js
+++ templates/components/plugin.js
@@ -9,7 +9,7 @@ const components = {
   const magicComments = [
     `webpackChunkName: "${c.chunkName}"`,
     c.prefetch === true || typeof c.prefetch === 'number' ? `webpackPrefetch: ${c.prefetch}` : false,
-    c.preload === true || typeof c.preload === 'number' ? `webpackPreload: ${c.prefetch}` : false,
+    c.preload === true || typeof c.preload === 'number' ? `webpackPreload: ${c.preload}` : false,
   ].filter(Boolean).join(', ')
 
   return `  ${c.pascalName.replace(/^Lazy/, '')}: () => import('../${relativeToBuild(c.filePath)}' /* ${magicComments} */).then(c => wrapFunctional(${exp}))`
